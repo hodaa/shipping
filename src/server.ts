a543eb910b4452479e6a270b2ee8fd36d6267ffc
hodaa <hoda.hussin@gmail.comm>
2026-06-05 06:29:47 +0200

    add binding to all netword

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,6 @@ app.post("/ship", async (req, res) => {
 });
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(3000, '0.0.0.0', () => {
     console.log("server started");
 });
